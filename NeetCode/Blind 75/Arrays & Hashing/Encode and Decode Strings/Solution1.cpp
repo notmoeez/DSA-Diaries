9faa776b73d79bd611faacda9a2b76bdd40e9beb
Abdul Moez <102456789+notmoeez@users.noreply.github.com>
2024-09-23 21:36:31 +0500

    Uploaded solution to encode decode strings

--- NeetCode/Blind 75/Arrays & Hashing/Encode and Decode Strings/Solution1.cpp
+++ NeetCode/Blind 75/Arrays & Hashing/Encode and Decode Strings/Solution1.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string combined = "";
+        int len = strs.size();
+        for(int i = 0;i < len;i++){
+            combined += to_string(strs[i].length()) + "@" + strs[i];
+        }
+        cout<<combined;
+        return combined;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> strs;
+        int i = 0;
+        int j;
+        while(i<s.length()){
+            j = i;
+            while(s[j]!='@'){
+                j++;
+            }
+            int len = stoi(s.substr(i,j-i));
+            string word = s.substr(j+1,len);
+            strs.push_back(word);
+            i = j+len+1;
+        }
+        
+        return strs;
+    }
+};
 
