31c811ed985bc8067124bb9b1bca69e6cc7523a8
Abdul Moez <102456789+notmoeez@users.noreply.github.com>
2024-09-23 15:16:34 +0500

    Added longest consecutive sequence solution

--- NeetCode/Blind 75/Arrays & Hashing/Longest Consecutive Sequence/Solution1.cpp
+++ NeetCode/Blind 75/Arrays & Hashing/Longest Consecutive Sequence/Solution1.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+        if(nums.size()==0)return 0;
+
+        unordered_map<int,bool> res;
+
+        int len=0;
+
+        auto minmax = minmax_element(nums.begin(), nums.end());
+        int min=*minmax.first;
+        int max=*minmax.second;
+
+        for(int i=min;i<=max;i++){
+            res[i]=false;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            res[nums[i]]=true;
+        }
+
+        int maxLen=0;
+        for(int i=min;i<=max;i++){
+            if(res[i]==1){
+                len++;
+            }
+            else{
+                if(maxLen<len)maxLen=len;
+                len=0;
+            }
+        }
+
+        if(maxLen>len)
+            return maxLen;
+        else
+            return len;
+    
+    }
+};
 
